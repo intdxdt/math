c41074dcf9f298e7528f45b0db18cd71d4b30d51
Titus Tienaah <titus.intdxdt@gmail.com>
2017-11-03 12:06:00 -0300

    consts

--- math.go
+++ math.go
@@ -5,12 +5,14 @@ import (
 )
 
 const (
-	PRECISION = 12
-	EPSILON   = 1.0e-12
-	Pi        = math.Pi
-	Tau       = 2.0 * Pi
-	Sqrt2     = math.Sqrt2
-	E         = math.E
+	PRECISION              = 12
+	EPSILON                = 1.0e-12
+	Pi                     = math.Pi
+	Tau                    = 2.0 * Pi
+	Sqrt2                  = math.Sqrt2
+	E                      = math.E
+	MaxFloat64             = math.MaxFloat64
+	SmallestNonzeroFloat64 = math.SmallestNonzeroFloat64
 )
 
 const (
